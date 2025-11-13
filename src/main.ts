3d6bb88b2d71950d3c02cc30a68b7de34b09f67d
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-11-13 10:02:30 +0900

    ollama 설치
    - evaluation 폴더 생성
    - ollama 설치
    - ollama 응답 테스트
    - 환경 변수 설정
    
    resolve: #1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { INestApplication } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/app.module";
+import { Env } from "@/config/env.config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService<Env, true>);
+  const port: number = configService.get("PORT", { infer: true });
+
+  await app.listen(port);
 }
 
 bootstrap();
