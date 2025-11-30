b61abc39aa77f090483285556e5f473a3307e52e
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-11-30 16:10:53 +0900

    배포 경로 설정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "backend-ts",
-      script: "dist/main.js",
+      script: "dist/src/main.js",
       cwd: "/home/prepair/prepair/backend-ts", // 라즈베리파이 배포 경로
       instances: 1, // 라즈베리파이는 싱글 인스턴스 권장
       exec_mode: "fork",
